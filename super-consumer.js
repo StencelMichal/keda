0fde551c63b0de07343f45519f38b6cdd88c7ed0
Michal Skwara <michal.skwara@fingoweb.com>
2023-05-21 23:00:10 +0200

    Add endpoint for toggling workload config

--- super-consumer.js
+++ super-consumer.js
@@ -1,4 +1,22 @@
 const { Kafka } = require('kafkajs');
+const express = require('express');
+const app = express();
+const port = 3000;
+
+let isSleep = false;
+
+app.get('/config/toggle-workload', (req, res) => {
+  isSleep = !isSleep;
+  res.send(`New config is: Sleep = ${isSleep}`);
+});
+
+app.listen(port, () => {
+  console.log(`Super app listening on port ${port}`);
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const topic = 'test-topic';
 const groupId = 'super-group-id';
@@ -17,11 +35,17 @@ const run = async () => {
 
   await consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
-      console.log({
+      console.log('Message data: ', {
+        topic,
         partition,
         offset: message.offset,
         value: message.value.toString(),
       });
+      if (isSleep) {
+        await sleep(10 * 1000);
+      } else {
+        console.log('Some calculation (Not implemented!)');
+      }
     },
   });
 };
