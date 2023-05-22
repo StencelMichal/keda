29f49d2ef49b141f04d3dbd28293abf6a1acc9ae
mikolaj <bulmikolaj@interia.pl>
2023-05-22 10:24:17 +0200

    fix scaler connectivity

--- charts/setup.sh
+++ charts/setup.sh
@@ -1,6 +1,7 @@
-kubectl apply -f kreda-namespace.yml
-kubectl config set-context --current --namespace=kreda
+kubectl apply -f keda-namespace.yml
+kubectl config set-context --current --namespace=keda
 kubectl apply -f zookeeper.yml
 kubectl apply -f kafka.yml
 kubectl apply -f consumer.yml
-
+kubectl apply -f https://github.com/kedacore/keda/releases/download/v2.10.1/keda-2.10.1.yaml
+kubectl apply -f kafka-scaler.yml
